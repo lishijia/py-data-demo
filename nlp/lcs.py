c8cb691dfed5b689c0afcc047af21fa48a770ff9
lishijia <shijia.li@hotmail.com>
2018-09-24 12:39:40 +0800

    lcs

--- nlp/lcs.py
+++ nlp/lcs.py
@@ -1,27 +1,29 @@
-b = 'ABCBDAB'
-a = 'BDCABA'
-
 def lcs(a, b):
     n = len(a)
     m = len(b)
-    l = [[0] * (m+1)] * (n+1)
+
     mean_len = (m+n)/2.0
-    # l = [[0]*(m+1) for i in range(n+1)]
+    l = [[0]*(m+1) for i in range(n+1)]
+    # l = [[0] * (m+1)] * (n+1)
     for i in range(n+1)[1:]:
         for j in range(m+1)[1:]:
             if a[i-1] == b[j-1]:
                 l[i][j] = l[i-1][j-1] + 1
             else:
                 l[i][j] = max(l[i-1][j], l[i][j-1])
+
+    print l[-1][-1]
+    print mean_len
     print l[-1][-1]/mean_len
     print l
 
-lcs(a, b)
-# l = [ [0] * (5+1) ] * 4
-#
-# print l
 
-l = [[0]*(3+1) for i in range(4+1)]
-ll = [ [0] * (3+1) ] * 5
+l = [[0] * (7+1)] * (6+1)
+ll = [[0]*(7+1) for i in range(6+1)]
 print l
 print ll
+
+a = 'BDCABA'
+b = 'ABCBDAB'
+lcs(a, b)
+
