6ab805572d7173f5366287ed4eb89277fb17b3cf
lishijia <shijia.li@hotmail.com>
2018-10-07 22:32:14 +0800

    mapreduce word count

--- mapreduce/map.py
+++ mapreduce/map.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
-
 import sys
-
 for line in sys.stdin:
     ss = line.strip().split(' ')
     for word in ss:
-        print "%s\t%d" % (word,1)
+        print "%s\t%d" % (word, 1)
--- mapreduce/reduce.py
+++ mapreduce/reduce.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python
 # coding=utf-8
 import sys
-
-cur_word=None
-cur_sum=0
-
+cur_word = None
+cur_sum = 0
 for line in sys.stdin:
-    ss=line.strip().split('\t')
-    if len(ss)!=2:
+    ss = line.strip().split('\t')
+    if len(ss) != 2:
         continue
-
-    word,value=ss
+    word, value = ss
     if cur_word == None:
-        cur_word=word
-
+        cur_word = word
     if cur_word == word:
-        cur_sum+=(int(value))
+        cur_sum += (int(value))
     else:
-        print "%s\t%d" % (cur_word,cur_sum)
-
-        cur_word=word
-        cur_sum=(int(value))
-
-print  "%s\t%d" % (cur_word,cur_sum)
+        print "%s\t%d" % (cur_word, cur_sum)
+        cur_word = word
+        cur_sum = (int(value))
+print "%s\t%d" % (cur_word, cur_sum)
