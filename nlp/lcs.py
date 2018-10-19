1a110a9cc65571533f4ea2c16b49f47c14abaefb
lishijia <shijia.li@hotmail.com>
2018-10-19 19:17:53 +0800

    lcs

--- nlp/lcs.py
+++ nlp/lcs.py
@@ -1,29 +1,29 @@
+# coding=utf-8
 def lcs(a, b):
+    # a字符的长度
     n = len(a)
+    # b字符的长度
     m = len(b)
-
-    mean_len = (m+n)/2.0
+    # 声明一个二维数组默认值全部为0
+    # 外层长度为 a（n）字符的长度+ 1
+    # 内层长度为 b（m）字符的长度+ 1
+    # data[n][m] 即 n行m列
     l = [[0]*(m+1) for i in range(n+1)]
-    # l = [[0] * (m+1)] * (n+1)
+
+    # 从1开始循环到n+1行
+    # 从1开始循环到m+1列
+
+    # 如果相同，则取对角数值+1
+    # 如果不相同，则取对角右和下的最大值
     for i in range(n+1)[1:]:
         for j in range(m+1)[1:]:
             if a[i-1] == b[j-1]:
                 l[i][j] = l[i-1][j-1] + 1
             else:
                 l[i][j] = max(l[i-1][j], l[i][j-1])
-
-    print l[-1][-1]
-    print mean_len
-    print l[-1][-1]/mean_len
-    print l
-
-
-l = [[0] * (7+1)] * (6+1)
-ll = [[0]*(7+1) for i in range(6+1)]
-lll = [0] * (7+1)
-print l
-print ll
-print lll
+    print
+    for data in l:
+        print data
 
 a = 'BDCABA'
 b = 'ABCBDAB'
